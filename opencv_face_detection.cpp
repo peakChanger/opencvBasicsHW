a646e543d15422b95fca0f9011d55f9267ec7a75
peakChanger <33837863+peakChanger@users.noreply.github.com>
2021-05-26 14:05:34 +0800

    finish chapter2

--- opencv_face_detection.cpp
+++ opencv_face_detection.cpp
@@ -10,6 +10,7 @@ using namespace cv;
 
 void chapter_one(int choose);//第一章教學 Read Image Video and Webcam
 void chapter_two();//第二章教學 Basic Function
+void chapter_three();//Resize and Crop
 
 void read_image_and_show(string filePath);// 讀取照片
 void read_video_and_play(string filePath);//讀取影片
@@ -20,7 +21,7 @@ void read_webCam_and_show(int deviceID);//讀取網路攝影機
 int main()
 {
 	//chapter_one(0);//(0照片，1影片，2網路攝影機)
-	chapter_two();
+	//chapter_two();
 }
 
 void chapter_one(int choose) {
@@ -51,19 +52,35 @@ void chapter_one(int choose) {
 void chapter_two() {
 	string imageFile = ".\\Data\\ic_vanilla.png";
 	Mat img = imread(imageFile);
+	Mat imgGray, imgBlur, imgCanny, imgDilate, imgErode;
 
 	//灰階
-	Mat imgGray;
 	cvtColor(img, imgGray, COLOR_BGR2GRAY);
 
 	//高斯模糊
-	Mat imgBlur;
-	GaussianBlur(img, imgBlur, Size(7, 7),5,0);
-	
-	imshow("Original",img);
+	GaussianBlur(img, imgBlur, Size(3, 3), 3, 0);
+
+	//Canny 邊緣偵測，通常在高斯模糊後進行邊緣抓取
+	Canny(imgBlur, imgCanny, 50, 150);
+
+	//Dilate 影像膨脹
+	Mat kernel = getStructuringElement(MORPH_RECT,Size(3,3));//增加Size增加找到的量，反之亦然
+	dilate(imgCanny, imgDilate, kernel);
+
+	//Erode 影像侵蝕
+	erode(imgDilate,imgErode,kernel);
+
+	imshow("Original", img);
 	imshow("Gray Img", imgGray);
 	imshow("Gaussian Blur", imgBlur);
+	imshow("Canny", imgCanny);
+	imshow("dilate", imgDilate);
+	imshow("Erode", imgErode);
 	waitKey(0);
+}
+
+void chapter_three() {
+	
 }
 
 void read_image_and_show(string filePath) {
